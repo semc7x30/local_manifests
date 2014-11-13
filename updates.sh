d8dbe52e6cb9db21c62c9002928f1a51d04041b5
MardonHH <markusmenzel@gmail.com>
2014-11-13 11:20:11 +0100

    its cm-12.0

--- updates.sh
+++ updates.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-if [ -f ~/bin/paths-11.0.sh ]; then
-	source ~/bin/paths-11.0.sh
+if [ -f ~/bin/paths-12.0.sh ]; then
+	source ~/bin/paths-12.0.sh
 fi
 
 if [ "${android}" = "" ]; then
