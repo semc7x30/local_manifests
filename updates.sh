5ac5e1e83a80982d0fd14fb7fb230b99cc4c011c
Michael Bestas <mikeioannina@gmail.com>
2015-04-01 02:24:36 +0300

    Add more cherries
    Change-Id: I402ca91026b29c013f858e9cd4efd4df00f3f9b5

--- updates.sh
+++ updates.sh
@@ -30,6 +30,36 @@ cherries+=(CM_84179)
 # vold: add ro.vold.umsdirtyratio property
 cherries+=(CM_88635)
 
+# SurfaceFlinger: Featurize libuiblur module for OSS build
+cherries+=(CM_91461)
+
+# bootloader: Silence /misc partition error
+cherries+=(CM_92919)
+
+# Fix Droid and animation color in recovery mode
+cherries+=(CM_92652)
+
+# Fix recovery image text rendering.
+cherries+=(CM_92693)
+
+# sr: Fix all the graphics issues
+cherries+=(CM_92694)
+
+# Revert old BGRA support patches
+cherries+=(CM_92725)
+
+# minui: Performance improvements
+cherries+=(CM_92726)
+
+# sr: Fix the header image
+cherries+=(CM_92765)
+
+# sr: Clean up navigation
+cherries+=(CM_92869)
+
+# mogami: Fix BT deep sleep
+cherries+=(LX_285)
+
 if [ -z $cherries ]; then
     echo -e "Nothing to cherry-pick!"
 else
