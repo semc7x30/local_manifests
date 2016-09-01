3781a520b4870b6ed77b97ad5ea922c284c13b4b
MardonHH <markusmenzel@gmail.com>
2016-09-01 13:44:17 +0300

    update: updates.sh
    Change-Id: I2d1dfcb8d9391f219ce882ec6fce0c7737dcce40

--- updates.sh
+++ updates.sh
@@ -12,6 +12,30 @@ if [ -f ${android}/updates-local.sh ]; then
     source ${android}/updates-local.sh
 fi
 
+# sad selinux 2002
+cherries+=(CM_158521)
+
+# linker: Make text relocations denial optional
+cherries+=(CM_158551)
+
+# Revert "Revert "Reenable support for non-PIE executables""
+cherries+=(CM_158950)
+
+# arm: Allow disabling PIE for dynamically linked executables
+cherries+=(CM_158951)
+
+# msm7x30: Allow text relocations
+cherries+=(LX_1348)
+
+# msm7x30: Switch cpu variant to cortex-a8
+cherries+=(LX_1349)
+
+# msm7x30: Reduce recovery resource density
+cherries+=(LX_1350)
+
+# policy_hal: Update for N
+cherries+=(LX_1353)
+
 if [ -z $cherries ]; then
     echo -e "Nothing to cherry-pick!"
 else
