3ad031bccdd5daf265f7ac6c6bbab479ae56d8ec
MardonHH <markusmenzel@gmail.com>
2016-09-01 13:44:17 +0300

    update: updates.sh
    Change-Id: I2d1dfcb8d9391f219ce882ec6fce0c7737dcce40

--- updates.sh
+++ updates.sh
@@ -12,6 +12,15 @@ if [ -f ${android}/updates-local.sh ]; then
     source ${android}/updates-local.sh
 fi
 
+# Revert "Revert "Reenable support for non-PIE executables""
+cherries+=(CM_158950)
+
+# arm: Allow disabling PIE for dynamically linked executables
+cherries+=(CM_158951)
+
+# fix: use SVELTE instead of jemalloc
+cherries+=(LX_1325)
+
 if [ -z $cherries ]; then
     echo -e "Nothing to cherry-pick!"
 else
