7e095459b7a8eb640e3ac3e1eeb1746a9b078bcd
Adrian DC <radian.dc@gmail.com>
2017-01-16 12:51:35 +0100

    [DNM] updates.sh: Holder of all patches for LegacyXperia
     * This commit is held here to isolate a list
        of all unmerged patches involved for LX devices
    
     * It is not meant to be merged or reviewed in any way
        or at any time, just updated to ease automated compilations
    
    Change-Id: I2ea887f460e04460744344f84afc31dff02346b7

--- updates.sh
+++ updates.sh
@@ -24,6 +24,55 @@ cherries+=(LAOS_1175)
 # arm: Allow disabling PIE for dynamically linked executables
 cherries+=(LAOS_2643)
 
+########################################################################
+
+# [DNM] updates.sh: Holder of all patches for LegacyXperia
+cherries+=(LX_1563)
+
+# temp cam source fix for semc2011
+cherries+=(LAOS_62448)
+
+# bootanimation: Adapt for small display sizes
+cherries+=(LAOS_156542)
+
+# init: set default interactive max cpu clock @1,4GHz
+cherries+=(LX_1473)
+
+# media_profiles: Enable a working true 720p mode
+cherries+=(LX_1474)
+
+# [DNM] msm7x30-common: Enable call recording
+cherries+=(LX_1519)
+
+# usb: mtp: increase RX transfer length to 1M
+cherries+=(LX_1547)
+
+# msm7x30-common: recovery: Provide TWRP 3.0.x configurations
+cherries+=(LX_1550)
+
+# Failed to load kernel modules
+cherries+=(LX_1551)
+
+# Reduce zRam size to 64 mb
+cherries+=(LX_1561)
+
+# Change the name of torch overlay parameter
+cherries+=(LX_1562)
+
+# Revert "msm7x30-common: Enable adaptive LMK"
+cherries+=(LX_1564)
+
+# Do not set ro.config.max_starting_bg
+cherries+=(LX_1565)
+
+# Reduce background apps limit to 8
+cherries+=(LX_1566)
+
+# Disable bootanimation preload and texture cache
+cherries+=(LX_1567)
+
+########################################################################
+
 if [ -z $cherries ]; then
     echo -e "Nothing to cherry-pick!"
 else
