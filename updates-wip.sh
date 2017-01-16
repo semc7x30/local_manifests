bae788c3c85206d9686832e01d8847608c3bdcbb
Adrian DC <radian.dc@gmail.com>
2017-01-16 12:51:35 +0100

    [DNM] updates: Holder of all WIP patches for LegacyXperia
     * This commit is held here to isolate a list
        of all unmerged patches involved for LX devices
    
     * It is not meant to be merged or reviewed in any way
        or at any time, just updated to ease automated compilations
    
    Change-Id: I2ea887f460e04460744344f84afc31dff02346b7

---
+++ updates-wip.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# [DNM] updates.sh: Holder of all patches for LegacyXperia
+cherries+=(LX_1563)
+
+# temp cam source fix for semc2011
+#cherries+=(LAOS_62448)
+
+# [DNM] msm7x30-common: Enable call recording
+#cherries+=(LX_1519)
+
+# TWRP changes
+cherries+=(LX_1550)
+cherries+=(LX_1585)
+cherries+=(OMNI_21655)
+cherries+=(OMNI_22096)
+
+# init: override default minfree values for LMK
+cherries+=(LX_1608)
+
+# msm7x30-common: Enable dex preoptimization
+cherries+=(LX_1609)
