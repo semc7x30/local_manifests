0682038c5b322ea967a99b96c50f13b7ad94fdec
Adrian DC <radian.dc@gmail.com>
2017-01-16 12:51:35 +0100

    [DNM] updates: Holder of all WIP patches for LegacyXperia
     * This commit is held here to isolate a list
        of all unmerged patches involved for LX devices
    
     * It is not meant to be merged or reviewed in any way
        or at any time, just updated to ease automated compilations
    
    Change-Id: I2ea887f460e04460744344f84afc31dff02346b7

---
+++ updates-wip.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# [DNM] updates.sh: Holder of all patches for LegacyXperia
+cherries+=(LX_1563)
+
+# temp cam source fix for semc2011
+cherries+=(LAOS_62448)
+
+# init: set default interactive max cpu clock @1,4GHz
+cherries+=(LX_1473)
+
+# media_profiles: Enable a working true 720p mode
+cherries+=(LX_1474)
+
+# msm7x30-common: Use Camera HAL1
+cherries+=(LX_1481)
+
+# [DNM] msm7x30-common: Enable call recording
+cherries+=(LX_1519)
+
+# usb: mtp: increase RX transfer length to 1M
+# cherries+=(LX_1547)
+
+# msm7x30-common: recovery: Provide TWRP 3.0.x configurations
+cherries+=(LX_1550)
+
+# Failed to load kernel modules
+# cherries+=(LX_1551)
+
+# Disable bootanimation preload and texture cache
+cherries+=(LX_1567)
